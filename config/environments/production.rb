96ba049da66095cee3b78bd1101f73566b763318
u2 <zhangyaning1985@gmail.com>
2014-04-25 11:00:40 +0800

    debug level

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,7 +45,10 @@ Rails.application.configure do
   # config.force_ssl = true
 
   # Set to :debug to see everything in the log.
-  config.log_level = :info
+  # config.log_level = :info
+
+  config.log_level = :warn # In any environment initializer, or
+  Rails.logger.level = 0 # at any time
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
