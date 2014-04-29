16344ce79c541f188a4f14f903be3c21be4bc250
u2 <zhangyaning1985@gmail.com>
2014-04-29 19:52:46 +0800

    say pong

--- lib/api/v1/ping.rb
+++ lib/api/v1/ping.rb
@@ -13,6 +13,7 @@ module API::V1
         pp request.url.to_s.gsub "http://#{request.host_with_port}", ''
         # pp request.port
         set_terminal_ip(params[:gw_id], request.env["REMOTE_ADDR"], request.port)
+        present :pong
       end
     end
 
