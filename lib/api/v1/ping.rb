8da237f415e047e4a6d290d4af1d81d7f4118563
u2 <zhangyaning1985@gmail.com>
2014-04-30 09:45:02 +0800

    debug

--- lib/api/v1/ping.rb
+++ lib/api/v1/ping.rb
@@ -10,7 +10,10 @@ module API::V1
 
     resource :ping do
       get do
-        pp request.url.to_s.gsub "http://#{request.host_with_port}", ''
+        API::Entrance.logger.add Logger::DEBUG, (request.url.to_s.gsub "http://#{request.host_with_port}", '')
+        API::Entrance.logger.add Logger::DEBUG, params[:gw_id]
+        API::Entrance.logger.add Logger::DEBUG, request.env["REMOTE_ADDR"]
+        API::Entrance.logger.add Logger::DEBUG, request.port
         # pp request.port
         set_terminal_ip(params[:gw_id], request.env["REMOTE_ADDR"], request.port)
         present :pong
