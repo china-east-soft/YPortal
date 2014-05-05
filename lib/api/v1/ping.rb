b3a233cc7adccec5c2e466c5ad784957d7fadd1f
u2 <zhangyaning1985@gmail.com>
2014-05-05 09:53:40 +0800

    debug

--- lib/api/v1/ping.rb
+++ lib/api/v1/ping.rb
@@ -8,20 +8,13 @@ module API::V1
       requires :gw_id, type: String, regexp: /\A([a-fA-F0-9]{2}:){5}[a-fA-F0-9]{2}\z/
     end
 
-    resource :ping do
-      get do
-        API::Entrance.logger.add Logger::DEBUG, (request.url.to_s.gsub "http://#{request.host_with_port}", '')
-        API::Entrance.logger.add Logger::DEBUG, params[:gw_id]
-        API::Entrance.logger.add Logger::DEBUG, request.env["REMOTE_ADDR"]
-        API::Entrance.logger.add Logger::DEBUG, request.port
-        API::Entrance.logger.add Logger::DEBUG, request.optional_port
-        API::Entrance.logger.add Logger::DEBUG, request.server_port
-        API::Entrance.logger.add Logger::DEBUG, request.standard_port
-        API::Entrance.logger.add Logger::DEBUG, request.port
-        # pp request.port
-        set_terminal_ip(params[:gw_id], request.env["REMOTE_ADDR"], request.port)
-        present :pong
-      end
+    get :ping do
+      API::Entrance.logger.add Logger::DEBUG, (request.url.to_s.gsub "http://#{request.host_with_port}", '')
+      API::Entrance.logger.add Logger::DEBUG, params[:gw_id]
+      API::Entrance.logger.add Logger::DEBUG, request.env["REMOTE_ADDR"]
+
+      set_terminal_ip(params[:gw_id], request.env["REMOTE_ADDR"], request.port)
+      present :pong
     end
 
   end
