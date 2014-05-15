a8384cab796f718ce44f0be3337ff6bd48809ea1
u2 <zhangyaning1985@gmail.com>
2014-05-15 09:59:41 +0800

    no block udp

--- test/models/account_signin_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class AccountSigninTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/merchant_info_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class MerchantInfoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
