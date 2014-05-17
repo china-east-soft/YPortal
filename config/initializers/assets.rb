4a0bff3edd4673b34b0ad1c17f42bab2ffc7d67f
u2 <zhangyaning1985@gmail.com>
2014-05-17 17:49:57 +0800

    style

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,4 +3,4 @@ Rails.application.config.assets.paths << Rails.root.join("app", "assets", "fonts
 Rails.application.config.assets.precompile += %w(.svg .eot .woff .ttf)
 
 Rails.application.config.assets.precompile += %w( admin.css devise.css wifi.css)
-Rails.application.config.assets.precompile += %w( admin.js devise.js wifi.js jquery.capSlide.js)
+Rails.application.config.assets.precompile += %w( admin.js devise.js wifi.js jquery.capSlide.js html5shiv.js respond.min.js)
