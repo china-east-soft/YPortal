8251e5952ec10ce8f98962fa64a67e800f943f1d
u2 <zhangyaning1985@gmail.com>
2014-06-05 10:00:41 +0800

    develop deploy config file

--- config/environments/develop.rb
+++ config/environments/develop.rb
@@ -85,5 +85,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.default_url_options = { :host => 'portal.cloudchain.cn' }
+  config.default_url_options = { :host => 'portal.cloudchain.co' }
 end
