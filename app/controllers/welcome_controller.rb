975c6313bf209f9bd675c3d7d4ffa08e3a02a7e0
u2 <zhangyaning1985@gmail.com>
2014-06-06 15:20:28 +0800

    send verify code fix

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -16,7 +16,7 @@ class WelcomeController < ActionController::Base
         auth_message = AuthMessage.where(mobile: params[:account_mobile], category: 0).first_or_initialize
       end
     end
-    if auth_message.save! && auth_message.send_result > 0
+    if auth_message && auth_message.save! && auth_message.send_result > 0
       render action: :generate_verify_code
     end
   end
