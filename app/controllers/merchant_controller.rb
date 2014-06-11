ae8e620d774dc08bab2da162c339582672a44c38
kailaichao <klc1989110@gmail.com>
2014-06-11 18:46:22 +0800

    merchant registation from terminal

--- app/controllers/merchant_controller.rb
+++ app/controllers/merchant_controller.rb
@@ -3,8 +3,39 @@ class MerchantController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :authenticate_merchant!
+  before_filter :authenticate_merchant!, except: :link_from_terminal
   layout 'merchant'
+
+
+  def link_from_terminal
+    mid = params[:mid]
+    if params[:mid].present?
+      #已登录
+      if current_merchant
+        #mid被添加过
+        if Terminal.find_by mid: mid
+          redirect_to merchant_root_url
+
+        #mid未被添加过
+        else
+          redirect_to new_merchant_terminal_url(mid: mid)
+        end
+
+      #未登录
+      else
+        #mid被添加过
+        if Terminal.find_by mid: mid
+          redirect_to new_merchant_session_path
+
+        #mid未被添加过
+        else
+          redirect_to new_merchant_registration_path(mid: mid)
+        end
+      end
+    else
+      redirect_to merchant_root_url
+    end
+  end
 
   def require_merchant
     unless current_merchant
