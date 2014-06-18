8c9e04217e73717aa5dc887bbc036f6c39939170
u2 <zhangyaning1985@gmail.com>
2014-06-18 16:10:24 +0800

    landing api

--- lib/api/v3/landings.rb
+++ lib/api/v3/landings.rb
@@ -6,7 +6,8 @@ module API::V3
       
       get :current do
         landings = Landing.where("(start_at <= ? and end_at >= ?) or start_at >= ?", Date.today.beginning_of_day, Date.today.end_of_day, Date.today.end_of_day).all
-        present landings.map{|i| {cover: i.cover.url, url: i.url, start_at: i.start_at, end_at: i.end_at } }
+        present :result, true
+        present :message, landings.map{|i| {cover: i.cover.url, url: i.url, start_at: i.start_at, end_at: i.end_at } }
       end
 
     end
