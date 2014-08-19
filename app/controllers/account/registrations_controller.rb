6a60cafdbe331eb89e71e892c48bd9cead1cd410
nowherekai <klc1989110@gmail.com>
2014-08-19 18:00:47 +0800

    fix bug

--- app/controllers/account/registrations_controller.rb
+++ app/controllers/account/registrations_controller.rb
@@ -44,4 +44,9 @@ class Account::RegistrationsController < Devise::RegistrationsController
 
   end
 
+  private
+  def after_sign_up_path_for(resource_or_scope)
+    account_signing_path(mac: params[:mac], client_identifier: params[:client_identifier])
+  end
+
 end
