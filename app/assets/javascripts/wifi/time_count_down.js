913ef951fcea078e40545849733e76f3c8bc1fc5
nowherekai <klc1989110@gmail.com>
2014-08-25 16:53:24 +0800

    count time

--- app/assets/javascripts/wifi/time_count_down.js
+++ app/assets/javascripts/wifi/time_count_down.js
@@ -1,31 +1,37 @@
 function timeCountDown(countdown_tag,duration)
 {
-   
+
   // variables for time units
   var hours, minutes, seconds;
- 
+
   // get tag element
   var countdown = document.getElementById(countdown_tag);
-  
+
   // update the tag with id "countdown" every 1 second
   var refreshIntervalId = setInterval(function () {
     if(duration >= 0) {
       // find the amount of "seconds" between now and target
       var seconds_left = duration;
-       
+
       hours = parseInt(seconds_left / 3600);
       seconds_left = seconds_left % 3600;
-       
+
       minutes = parseInt(seconds_left / 60);
-       
+
       // format countdown string + set tag value
-      countdown.innerHTML = hours + " 小时 "
-      + minutes + " 分 ";     
+      if (hours > 0)
+      {
+        countdown.innerHTML = hours + " 小时 " + minutes + " 分 ";
+      }
+      else
+      {
+        countdown.innerHTML = minutes + " 分 ";
+      }
     } else {
       window.clearInterval(refreshIntervalId);
     }
-  
+
     duration = duration - 1;
-   
+
   }, 60000);
 }
