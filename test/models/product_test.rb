c4550372a0bb9fa8d00e122922d4be8a318f8429
nowherekai <klc1989110@gmail.com>
2014-09-11 09:34:02 +0800

    update test

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,22 +1,20 @@
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
+
   def setup
     @product = create(:product)
   end
 
   test "should create product" do
-    setup
     assert_not_nil @product
   end
 
   test "should respond to hot?" do
-    setup
     assert_respond_to @product, :hot?
   end
 
   test "shoud have name and price and desctiption" do
-    setup
     assert_not_nil @product.name
     assert_not_nil @product.price
     assert_not_nil @product.description
