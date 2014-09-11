98adce46bb0556ed9bf7681f1a864094647080cd
nowherekai <klc1989110@gmail.com>
2014-09-11 10:15:05 +0800

    update test

---
+++ test/factories/account.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :account do
+
+    mobile "13738091353"
+    type  0
+    password "12345678"
+    password_confirmation "12345678"
+
+  end
+end
---
+++ test/factories/auth_message.rb
@@ -0,0 +1,4 @@
+FactoryGirl.define do
+  factory :auth_message do
+  end
+end
