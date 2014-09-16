1efbd6874e90f057ac9b4197bf62bb2d9261be44
nowherekai <klc1989110@gmail.com>
2014-09-16 14:39:21 +0800

    program model test

--- test/models/program_test.rb
+++ test/models/program_test.rb
@@ -9,4 +9,31 @@ class ProgramTest < ActiveSupport::TestCase
     p2.valid?
     assert_not_nil p2.errors[:channel]
   end
+
+  test "comment count cache should increase" do
+    p = create(:program)
+    10.times do
+      c = create(:comment)
+      p.comments << c
+    end
+
+    p.reload
+    assert_equal p.comments_count,  p.comments.count
+  end
+
+  test "comment count cache should decrease" do
+    p = create(:program)
+    10.times do
+      c = create(:comment)
+      p.comments << c
+    end
+
+    p = Program.find_by(channel: p.channel)
+    p.comments.first.destroy
+    p.reload
+
+    assert_equal p.comments.count, 9
+    assert_equal p.comments_count,  p.comments.count
+  end
+
 end
