7a14d76b266276ec6b8e6cfa17275c37c1c7942e
nowherekai <klc1989110@gmail.com>
2014-09-17 11:06:32 +0800

    add query program name api and update channel regex
    * replace # with -, change regex in model and view javascript
    * add URI.escape of chinese in repec

--- app/models/program.rb
+++ app/models/program.rb
@@ -2,9 +2,8 @@ class Program < ActiveRecord::Base
   has_many :comments, dependent: :destroy
 
   #channel 四个字段唯一确定一个节目, 形式如： CMMB#12#28#杭州
-  CHANNEL_FORMAT = /\A\w+#(\d+#){2}\p{Word}+\Z/u
+  CHANNEL_FORMAT = /\A\w+\-(\d+\-){2}\p{Word}+\Z/u
   validates :channel, presence: true, uniqueness: true, format: {with: CHANNEL_FORMAT,
                                                                   message: "格式错误！"}
-
   validates_presence_of :name
 end
--- spec/api/comments_spec.rb
+++ spec/api/comments_spec.rb
@@ -3,8 +3,7 @@ require 'active_support/core_ext'
 
 describe API::V3, "comments", :type => :request do
   describe "POST /api/v3/comments/create.json" do
-    it "should return true" do
-      post "/api/v3/comments/create.json", mac: "dd:df:33:33", channel: "cctv-1", body: "here we go"
+    it "should return true" do post "/api/v3/comments/create.json", mac: "dd:df:33:33", channel: "cctv-1", body: "here we go"
       expect(response.status).to eq(201)
       expect(response.body).to eq('{"result":true}')
     end
@@ -19,9 +18,23 @@ describe API::V3, "comments", :type => :request do
   describe "GET /api/v3/comments/select.json" do
     let (:comment) { FactoryGirl.create(:comment) }
     it "should return comments" do
-      get "/api/v3/comments/select.json?channel=#{comment.channel}&&limit=1"
+      get "/api/v3/comments/select.json?channel=#{URI.escape(comment.channel)}&limit=1"
       expect(response.status).to eq(200)
       expect(response.body).to match(/true/)
+    end
+  end
+
+  describe "GET /api/v3/comments/program_name.json" do
+    let (:program) { FactoryGirl.create(:program) }
+
+    it "should return program name" do
+      get "/api/v3/comments/program_name.json?channel=#{URI.escape(program.channel)}"
+      expect(response.body).to match(/name/)
+    end
+
+    it "should return false if channel not exist" do
+      get "/api/v3/comments/program_name.json?channel=should-not-find"
+      expect(response.body).to match(/false/)
     end
   end
 end
