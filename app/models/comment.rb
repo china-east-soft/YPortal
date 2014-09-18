bb5e982baf18f5891b64fe97150e9caecaa422b6
nowherekai <klc1989110@gmail.com>
2014-09-18 15:34:01 +0800

    update comment api
    * limit time for 4 hour

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -9,9 +9,11 @@ class Comment < ActiveRecord::Base
   def self.comments_for_app(channel:, id: 0, limit: 20)
     #id == 0 present request for the newest record
     if id == 0
-      where(channel: channel).limit(limit)
+      where("created_at >= ?", Time.now - 4.hour).
+        where(channel: channel).limit(limit)
     else
-      where("channel = :channel AND id < :id", {channel: channel, id: id}).limit(limit)
+      where("created_at >= ?", Time.now - 4.hour).
+        where("channel = :channel AND id < :id", {channel: channel, id: id}).limit(limit)
     end
   end
 
