dd1b366eb0e00f1335877e74c942660f9aed63b3
nowherekai <klc1989110@gmail.com>
2014-09-24 14:50:19 +0800

    update comment model test
    * add test for parent-child
    * add test of ancestor

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -49,4 +49,36 @@ class CommentTest < ActiveSupport::TestCase
     end
   end
 
+  test "parent of comemnt" do
+    c1 = create(:comment)
+    c2 = create(:comment)
+    c2.parent = c1
+    c2.save
+
+    assert_equal c1, c2.parent
+  end
+
+  test "children of commetn" do
+    c1 = create(:comment)
+    children = (1..10).map {
+      Comment.create(body: "test", mac: c1.mac, channel: c1.channel, parent_id: c1.id)
+    }
+
+    assert_equal c1.children.to_a, children.reverse
+  end
+
+  test "ancestor" do
+    c1 = create(:comment)
+    assert c1.ancestor.empty?
+
+    c2 = build(:comment)
+    c2.parent = c1
+    c2.save
+    assert_equal c2.ancestor.to_a, [c1]
+
+    c3 = build(:comment)
+    c3.parent = c2
+    c3.save
+    assert_equal c3.ancestor.to_a, [c1, c2]
+  end
 end
