ff9033ed42b160e3f811720b51345cfb1959c04c
nowherekai <klc1989110@gmail.com>
2014-09-24 14:37:15 +0800

    add parent-child relationship for comment
    * use postgresql with recurion

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -17,4 +17,33 @@ class Comment < ActiveRecord::Base
     end
   end
 
+
+  #tree structrue(parent-child relationship)
+  belongs_to :parent, class_name: "Comment"
+  has_many :children, class_name: "Comment", foreign_key: "parent_id"
+
+  def ancestor
+    self.class.ancestor_for(self)
+  end
+
+  def self.ancestor_for(instance)
+    unscoped.where("#{table_name}.id in (#{ancestor_sql_for(instance)})").order("#{table_name}.id asc")
+  end
+
+  #use postgresql with recursive for performace
+  def self.ancestor_sql_for(instance)
+    ancestors_sql = <<-SQL
+   WITH RECURSIVE search_ancestor_tree(id, parent_id, path) as (
+     select id, parent_id, ARRAY[id] from #{table_name}
+     where id = #{instance.id}
+   UNION ALL
+     SELECT #{table_name}.id, #{table_name}.parent_id, path || #{table_name}.id
+     from search_ancestor_tree join #{table_name} on #{table_name}.id = search_ancestor_tree.parent_id where not #{table_name}.id = ANY(path)
+   )
+   select parent_id from search_ancestor_tree order by path
+   SQL
+  end
+
+
+
 end
