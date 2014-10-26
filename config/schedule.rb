897f203c1f2866a6e6740435f3a1f395a7daef1f
nowherekai <klc1989110@gmail.com>
2014-10-26 14:26:00 +0800

    update

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,22 +1,5 @@
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
 
-# Example:
-#
-# set :output, "/path/to/my/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
-
+set :environment, @environment
 every 1.day, :at => '4:30 am' do
   rake "activites:update_activity_status"
   rake "data:clear_test_vtokens"
@@ -26,17 +9,20 @@ every 10.days, :at => '5 am' do
   rake "redis:clear_invalid_keys"
 end
 
-if @environment == 'production'
+case @environment
+when 'production'
   every 1.day, at: '02:00 am' do
     command "cd /root/ && backup perform --trigger protal_cloudchain_cn_files_backup"
     command "cd /root/ && backup perform --trigger protal_cloudchain_cn_database_backup"
   end
-elsif @environment == 'testing'
+when 'testing'
   every 1.day, at: '02:00 am' do
     command "cd /root/ && backup perform --trigger protal_co_files_backup"
     command "cd /root/ && backup perform --trigger protal_co_database_backup"
   end
 end
+
+puts "debug:#{@environment}"
 
 every 7.days, :at => '03:00 am' do
   rake "log:clear"
