045d213e8b484383a6489315f2ca450482bba12f
nowherekai <klc1989110@gmail.com>
2014-12-10 17:10:16 +0800

    add block to relationship

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -3,4 +3,7 @@ class Relationship < ActiveRecord::Base
   belongs_to :followed, class_name: "User"
 
   validates_presence_of :follower_id, :followed_id
+
+  #状态， 单向关注， 相互关注， 拉黑
+  enum status: [:unidirection, :bidirectional, :block]
 end
