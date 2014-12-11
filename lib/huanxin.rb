2ce23f0430f302d640f0031885528ca74a8fac19
nowherekai <klc1989110@gmail.com>
2014-12-11 12:00:23 +0800

    huanxin restful op

--- lib/huanxin.rb
+++ lib/huanxin.rb
@@ -10,71 +10,215 @@ module Huanxin
 
   RESTFUL_API = %W[register_user unregister_user get_user_info]
 
-  #curl -X POST -i "https://a1.easemob.com/easemob-demo/chatdemo/users" -d '{"username":"jliu","password":"123456"}'
-  #RestClient.post 'http://example.com/resource', :param1 => 'one', :nested => { :param2 => 'two' }
-  def register_user(user)
-    username = Digest::MD5.hexdigest(user.id.to_s)
-    password = Digest::MD5.hexdigest(user.id.to_s.reverse)
-    nickname = user.name
-    begin
-      response = RestClient.post("#{DOMAIN}/#{ORG}/#{APP}/users",
-                                 { username: username, password: password,
-                                   nickname: nickname
-                                 }.to_json,
-                                 :content_type => :json,
-                                 :accept => :json
-                                )
-      if response.code == 200
-        body = JSON.parse(response.body)
-        if body["entities"].first["activated"] == true
-          user.username_huanxin = username
-          user.register_huanxin = true
-          user.save!
-        end
-      end
-    rescue => e
-      puts e.response
-    end
-  end
-
-  #curl -X DELETE -H "Authorization: Bearer YWMtSozP9jHNEeSQegV9EK5eAQAAAUlmBR2bTGr-GP2xNh8GhUCdKViBFgtox3M" -i  "https://a1.easemob.com/easemob-demo/chatdemoui/users/ywuxvxuir6"
-  def unregister_user(user)
-    username = Digest::MD5.hexdigest(user.id.to_s)
-    begin
-      response = RestClient.delete("#{DOMAIN}/#{ORG}/#{APP}/users/#{username}",
-                                   "Authorization" => "Bearer #{access_token}",
-                                   :accept => :json
-                                  )
-      if response.code == 200
-        body = JSON.parse(response.body)
-        if body["action"] == "delete"
-          user.username_huanxin = nil
-          user.register_huanxin = false
-          user.save!
-        end
-      end
-    rescue => e
-      puts e.response
-    end
-  end
-
-  #curl -X GET -H "Authorization: Bearer YWMtSozP9jHNEeSQegV9EK5eAQAAAUlmBR2bTGr-GP2xNh8GhUCdKViBFgtox3M" -i  "https://a1.easemob.com/easemob-demo/chatdemoui/users/ywuxvxuir6"
-  def get_user_info(user)
-    if user.respond_to? :id
+  #对应环信的用户体系集成
+  module UserSystem
+    #curl -X POST -i "https://a1.easemob.com/easemob-demo/chatdemo/users" -d '{"username":"jliu","password":"123456"}'
+    #RestClient.post 'http://example.com/resource', :param1 => 'one', :nested => { :param2 => 'two' }
+    def register_user(user)
       username = Digest::MD5.hexdigest(user.id.to_s)
-    else
-      username = user
+      password = Digest::MD5.hexdigest(user.id.to_s.reverse)
+      nickname = user.name
+      begin
+        response = RestClient.post("#{DOMAIN}/#{ORG}/#{APP}/users",
+                                  { username: username, password: password,
+                                    nickname: nickname
+                                  }.to_json,
+                                  :content_type => :json,
+                                  :accept => :json
+                                  )
+        if response.code == 200
+          body = JSON.parse(response.body)
+          if body["entities"].first["activated"] == true
+            user.username_huanxin = username
+            user.register_huanxin = true
+            user.save!
+          end
+        end
+      rescue => e
+        puts e.response
+      end
     end
 
-    begin
-      url = "#{DOMAIN}/#{ORG}/#{APP}/users/#{username}"
-      response = RestClient.get(url, "Authorization" => "Bearer #{access_token}", content_type: :json, accept: :json)
-      res = JSON.parse(response.body)
-      p res["entities"]
-    rescue => e
-      puts e.response
+    #curl -X DELETE -H "Authorization: Bearer YWMtSozP9jHNEeSQegV9EK5eAQAAAUlmBR2bTGr-GP2xNh8GhUCdKViBFgtox3M" -i  "https://a1.easemob.com/easemob-demo/chatdemoui/users/ywuxvxuir6"
+    def unregister_user(user)
+      username = Digest::MD5.hexdigest(user.id.to_s)
+      begin
+        response = RestClient.delete("#{DOMAIN}/#{ORG}/#{APP}/users/#{username}",
+                                    "Authorization" => "Bearer #{access_token}",
+                                    :accept => :json
+                                    )
+        if response.code == 200
+          body = JSON.parse(response.body)
+          if body["action"] == "delete"
+            user.username_huanxin = nil
+            user.register_huanxin = false
+            user.save!
+          end
+        end
+      rescue => e
+        puts e.response
+      end
+    end
+
+    #curl -X GET -H "Authorization: Bearer YWMtSozP9jHNEeSQegV9EK5eAQAAAUlmBR2bTGr-GP2xNh8GhUCdKViBFgtox3M" -i  "https://a1.easemob.com/easemob-demo/chatdemoui/users/ywuxvxuir6"
+    def get_user_info(user)
+      if user.respond_to? :id
+        username = Digest::MD5.hexdigest(user.id.to_s)
+      else
+        username = user
+      end
+
+      begin
+        url = "#{DOMAIN}/#{ORG}/#{APP}/users/#{username}"
+        response = RestClient.get(url, "Authorization" => "Bearer #{access_token}", content_type: :json, accept: :json)
+        res = JSON.parse(response.body)
+        p res["entities"]
+      rescue => e
+        puts e.response
+      end
+    end
+
+    #curl -X POST -H 'Authorization: Bearer YWMtwIRGSE9gEeSbpNnVBsIhiwAAAUon2XDyEBoBUk6Vg2xm8DZdVjxbhwm7XWY' -i  'https://a1.easemob.com/easemob-demo/chatdemoui/users/v3y0kf9arx/blocks/users' -d '{"usernames":["5cxhactgdj", "mh2kbjyop1"]}'
+    def block_user(user, blocked_user)
+      username = Digest::MD5.hexdigest(user.id.to_s)
+      blocked_username = Digest::MD5.hexdigest(blocked_user.id.to_s)
+      begin
+        response = RestClient.post("#{DOMAIN}/#{ORG}/#{APP}/users/#{username}/blocks/users",
+                                  { usernames: [blocked_username]
+                                  }.to_json,
+                                  "Authorization" => "Bearer #{access_token}",
+                                  :content_type => :json,
+                                  :accept => :json
+                                  )
+        if response.code == 200
+          p response
+
+          body = JSON.parse(response.body)
+          p body
+        end
+      rescue => e
+        puts e.response
+      end
+    end
+
+    #curl -X DELETE -H 'Authorization: Bearer YWMtwIRGSE9gEeSbpNnVBsIhiwAAAUon2XDyEBoBUk6Vg2xm8DZdVjxbhwm7XWY' -i  'https://a1.easemob.com/easemob-demo/chatdemoui/users/v3y0kf9arx/blocks/users/5cxhactgdj'
+    def unblock_user(user, blocked_user)
+      username = Digest::MD5.hexdigest(user.id.to_s)
+      blocked_username = Digest::MD5.hexdigest(blocked_user.id.to_s)
+      begin
+        response = RestClient.delete("#{DOMAIN}/#{ORG}/#{APP}/users/#{username}/blocks/users/#{blocked_username}",
+                                  "Authorization" => "Bearer #{access_token}",
+                                  :content_type => :json,
+                                  :accept => :json
+                                  )
+        if response.code == 200
+          p response
+
+          body = JSON.parse(response.body)
+          p body
+        end
+      rescue => e
+        puts e.response
+      end
+    end
+
+    #curl -X GET -H "Authorization: Bearer YWMtwIRGSE9gEeSbpNnVBsIhiwAAAUon2XDyEBoBUk6Vg2xm8DZdVjxbhwm7XWY" -i  "https://a1.easemob.com/easemob-demo/chatdemoui/users/v3y0kf9arx/blocks/users"
+    def get_blacklist(user)
+      username = Digest::MD5.hexdigest(user.id.to_s)
+      begin
+        response = RestClient.get("#{DOMAIN}/#{ORG}/#{APP}/users/#{username}/blocks/users",
+                                  "Authorization" => "Bearer #{access_token}",
+                                  :content_type => :json,
+                                  :accept => :json
+                                  )
+        if response.code == 200
+          p response
+
+          body = JSON.parse(response.body)
+          p body
+        end
+      rescue => e
+        puts e.response
+      end
     end
   end
+
+  module SendMessage
+    #curl -X GET -i -H "Authorization: Bearer YWMtxc6K0L1aEeKf9LWFzT9xEAAAAT7MNR_9OcNq-GwPsKwj_TruuxZfFSC2eIQ" "https://a1.easemob.com/easemob-demo/chatdemoui/users/zw123/status"
+    def get_user_status(user)
+      username = Digest::MD5.hexdigest(user.id.to_s)
+      begin
+        response = RestClient.get("#{DOMAIN}/#{ORG}/#{APP}/users/#{username}/status",
+                                  "Authorization" => "Bearer #{access_token}",
+                                  :content_type => :json,
+                                  :accept => :json
+                                  )
+        if response.code == 200
+          p response
+
+          body = JSON.parse(response.body)
+          p body
+        end
+      rescue => e
+        puts e.response
+      end
+    end
+
+    #curl -X POST -i -H "Authorization: Bearer YWMtxc6K0L1aEeKf9LWFzT9xEAAAAT7MNR_9OcNq-GwPsKwj_TruuxZfFSC2eIQ" "https://a1.easemob.com/easemob-demo/chatdemoui/messages" -d '{"target_type" : "users","target" : ["stliu1", "jma3", "stliu", "jma4"],"msg" : {"type" : "txt","msg" : "hello from rest"},"from" : "jma2", "ext" : {"attr1" : "v1","attr2" : "v2"} }'
+    def send_text_message(from_user, to_user, message)
+      from_username = Digest::MD5.hexdigest(from_user.id.to_s)
+      to_username = Digest::MD5.hexdigest(to_user.id.to_s)
+      begin
+        response = RestClient.post("#{DOMAIN}/#{ORG}/#{APP}/messages",
+                                  {
+                                    target_type: "users",
+                                    target: [to_username],
+                                    msg: { type: "txt", msg: message},
+                                    from: from_username
+                                  }.to_json,
+                                  "Authorization" => "Bearer #{access_token}",
+                                  :content_type => :json,
+                                  :accept => :json
+                                  )
+        if response.code == 200
+          p response
+
+          body = JSON.parse(response.body)
+          p body
+        end
+      rescue => e
+        puts e.response
+      end
+    end
+  end
+
+  module ChatHistroy
+    #curl -X GET -i -H "Authorization: Bearer YWMtxc6K0L1aEeKf9LWFzT9xEAAAAT7MNR_9OcNq-GwPsKwj_TruuxZfFSC2eIQ" "https://a1.easemob.com/easemob-demo/chatdemoui/chatmessages?ql=select+*+where+timestamp%3C1403164734226+and+timestamp%3E1403163586000"
+    def get_chat_histroy#(user)
+      # username = Digest::MD5.hexdigest(user.id.to_s)
+      begin
+        response = RestClient.get("#{DOMAIN}/#{ORG}/#{APP}/chatmessages",
+                                  "Authorization" => "Bearer #{access_token}",
+                                  :content_type => :json,
+                                  :accept => :json
+                                  )
+        if response.code == 200
+          p response
+
+          body = JSON.parse(response.body)
+          p body
+        end
+      rescue => e
+        puts e.response
+      end
+
+    end
+  end
+
+
+
+  include UserSystem
+  include SendMessage
 
   private
   def access_token
@@ -112,4 +256,5 @@ module Huanxin
       puts e.response
     end
   end
+
 end
